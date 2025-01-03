53e98199275987b62de340ecde3d08227380810d
Severin Buhler <sevi_buehler@hotmail.com>
2025-01-03 10:06:29 +0100

    updated to synonymsoft repo

--- build_docker.sh
+++ build_docker.sh
@@ -3,21 +3,33 @@
 export VERSION=$(cd server && cargo get package.version)
 cross build --release --package=pkdns --target=x86_64-unknown-linux-musl
 cross build --release --package=pkdns-cli --target=x86_64-unknown-linux-musl
-docker build --platform linux/amd64 -t sebu/pkdns:$VERSION -t sebu/pkdns:latest .
+docker build --platform linux/amd64 -t synonymsoft/pkdns:$VERSION -t synonymsoft/pkdns:latest .
 
 
 echo
-echo Built sebu/pkdns:$VERSION, sebu/pkdns:latest
-read -p "Do you want to publish the image to Docker Hub? [yN] " response
+read -p "Do you want to publish synonymsoft/pkdns:$VERSION to Docker Hub? [yN] " response
 echo
 case "$response" in
     [yY]|[yY][eE][sS])  # Accepts y, Y, yes, YES
 
-        docker push sebu/pkdns:$VERSION
-        docker push sebu/pkdns:latest
+        docker push synonymsoft/pkdns:$VERSION
         ;;
     *)
-        echo "Dont publish."
+        echo "Dont publish synonymsoft/pkdns:$VERSION."
+        exit 1
+        ;;
+esac
+
+echo
+read -p "Do you want to publish synonymsoft/pkdns:latest to Docker Hub? [yN] " response
+echo
+case "$response" in
+    [yY]|[yY][eE][sS])  # Accepts y, Y, yes, YES
+
+        docker push synonymsoft/pkdns:latest
+        ;;
+    *)
+        echo "Dont publish synonymsoft/pkdns:latest."
         exit 1
         ;;
 esac
