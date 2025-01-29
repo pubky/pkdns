80f3277dfc25e5523be91d13909f716ca8af22fb
Severin Alexander Bühler <8782386+SeverinAlexB@users.noreply.github.com>
2025-01-29 09:44:30 +0200

    chore: pkarr v2.3.1 (#97)
    * chore pkarr v2.3.1
    
    * removed unnecessary comments
    
    * fmt

--- cli/src/helpers.rs
+++ cli/src/helpers.rs
@@ -3,7 +3,7 @@ use pkarr::{PkarrClient, PkarrClientBuilder};
 /// Construct new pkarr client with no cache, no resolver, only DHT
 pub fn construct_pkarr_client() -> PkarrClient {
     PkarrClientBuilder::default()
-        // .maximum_ttl(0)
+        .maximum_ttl(0)
         .resolvers(None)
         .build()
         .unwrap()
