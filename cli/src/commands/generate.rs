b238e199b5eee7263c53da9df0b53adfefd13dcc
Severin Buhler <sevi_buehler@hotmail.com>
2025-08-04 13:34:15 +0200

    clippy

--- cli/src/commands/generate.rs
+++ cli/src/commands/generate.rs
@@ -3,6 +3,6 @@ use pkarr::Keypair;
 
 pub async fn cli_generate_seed(_matches: &ArgMatches) {
     let keypair = Keypair::random();
-    let encoded = hex::encode(&keypair.secret_key());
+    let encoded = hex::encode(keypair.secret_key());
     println!("{encoded}");
 }
